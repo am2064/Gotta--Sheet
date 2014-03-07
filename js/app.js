73f10b31c9124709ec88f32634a6715ef3fb4ef8
am2064 <am2064@nova.edu>
2014-03-07 07:52:27 -0500

    Dynamic views, began messing with pathfinder view

--- js/app.js
+++ js/app.js
@@ -3,14 +3,15 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
+		'ngRoute',
+		'myApp.filters',
+		'myApp.services',
+		'myApp.directives',
+		'myApp.controllers'
+		]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+	$routeProvider.when('/characters/:id', { templateUrl: '/Gotta--Sheet/partials/blank.html', controller: 'SheetsController'});
+	$routeProvider.when('/characters/', { templateUrl: '/Gotta--Sheet/partials/characters.html', controller: 'CharactersController'});
+	$routeProvider.when('/',{templateUrl: '/Gotta--Sheet/partials/home.html'});
+	$routeProvider.otherwise({templateUrl: '/Gotta--Sheet/partials/404.html'});
 }]);
--- js/controllers.js
+++ js/controllers.js
@@ -3,9 +3,26 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+controller('SheetsController', ['$scope','$http','$route','$routeParams','$compile', function($scope, $http, $route, $routeParams, $compile) {
+	$http.get("http://localhost:8000/api/v1/characters/"+$routeParams.id).then(
+		function(res){
+			$scope.character = res.data.character;
+			$route.current.templateUrl = "/Gotta--Sheet/partials/" + $scope.character.game.name + "/show.html";
+			$http.get($route.current.templateUrl).then(
+				function (msg) {
+					$('#views').html($compile(msg.data)($scope));
+				},
+				function(){
+					$route.current.templateUrl = "/Gotta--Sheet/partials/default/show.html";
+					$http.get($route.current.templateUrl).then(
+						function (msg) {
+							$('#views').html($compile(msg.data)($scope));
+						});
+				}
+				);
+		},
+		function(){
+			$("#views").html("Error");
+		});
 
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+}]);
